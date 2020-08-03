0d28ec70fc6c4f992eb2b48ea34b56102aea69fd
MizukiNumata <handle1997@gmail.com>
2020-08-03 14:28:02 +0900

    changed to php

---
+++ article/sec1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Sec. 1.</title>
+    <link rel="stylesheet" type="text/css" href="article.css">
+</head>
+
+<body>
+
+    <!-- sec1 -->
+    <?php include("sec1/001.html"); ?>
+    <?php include("sec1/002.html"); ?>
+    <?php include("sec1/003.html"); ?>
+    <?php include("sec1/003.html"); ?>
+    <?php include("sec1/004.html"); ?>
+    <?php include("sec1/005.html"); ?>
+    <?php include("sec1/006.html"); ?>
+    <?php include("sec1/007.html"); ?>
+    <?php include("sec1/008.html"); ?>
+    <?php include("sec1/009.html"); ?>
+    <?php include("sec1/010.html"); ?>
+    <?php include("sec1/011.html"); ?>
+    <?php include("sec1/012.html"); ?>
+    <?php include("sec1/013.html"); ?>
+    <?php include("sec1/014.html"); ?>
+    <?php include("sec1/015.html"); ?>
+    <?php include("sec1/016.html"); ?>
+
+</body>
+
+</html>
---
+++ article/sec2_ch1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Sec. 2 Ch. 1.</title>
+    <link rel="stylesheet" type="text/css" href="article.css" />
+  </head>
+
+  <body>
+    <!-- sec -->
+    <div class="column">
+      <div class="article" lang="en">
+        <h1>
+          SECTION II. <br />
+          SYNTAXIS CONVENIENTIAE AND SYNTAXIS RECTIONIS.
+        </h1>
+      </div>
+      <div class="article" lang="ja">
+        <h1>
+          hoge
+        </h1>
+      </div>
+    </div>
+
+    <hr />
+
+    <?php
+        include("sec2/019.html");
+    ?>
+  </body>
+</html>
