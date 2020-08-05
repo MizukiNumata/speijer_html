26a5d4b1f44ab06bfb3c4d63a0301d60091edf34
MizukiNumata <handle1997@gmail.com>
2020-08-06 05:05:09 +0900

    add 040

---
+++ article/sec2_ch3.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Sec. 2 Ch. 3.</title>
+    <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+    <div class="column">
+        <div class="article" lang="en">
+            <h2>
+                <span style="font-variant: small-caps;">Chapt.</span> III. Accusative.
+            </h2>
+        </div>
+        <div class="article" lang="ja">
+            <h2>
+                第3章 対格（accusative）
+            </h2>
+        </div>
+    </div>
+
+    <?php include("sec2/ch2/039.html"); ?>
+
+
+</body>
+
+</html>
