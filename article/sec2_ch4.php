900e850a4e064d185922148ea3eca0aae0a678e1
MizukiNumata <handle1997@gmail.com>
2020-08-09 01:08:16 +0900

    add 057

---
+++ article/sec2_ch4.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Sec. 2 Ch. 4.</title>
+    <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+    <div class="column">
+        <div class="article" lang="en">
+            <h2>
+                <span style="font-variant: small-caps;">Chapter</span> IV. Instrumental.
+            </h2>
+        </div>
+        <div class="article" lang="ja">
+            <h2>
+                第4章 具格（instrumental）
+            </h2>
+        </div>
+    </div>
+
+    <?php include("sec2/ch3/057.html"); ?>
+    <?php include("sec2/ch3/058.html"); ?>
+
+
+
+</body>
+
+</html>
