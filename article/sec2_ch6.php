cc584b0fe0a1829d323d721247edc6eec9ea1f7d
MizukiNumata <handle1997@gmail.com>
2020-09-14 22:34:49 +0900

    add 098

--- article/sec2_ch6.php
+++ article/sec2_ch6.php
@@ -32,6 +32,7 @@
     <?php include("sec2/ch6/095.html"); ?>
     <?php include("sec2/ch6/096.html"); ?>
     <?php include("sec2/ch6/097.html"); ?>
+    <?php include("sec2/ch6/098.html"); ?>
 
 
 </body>
