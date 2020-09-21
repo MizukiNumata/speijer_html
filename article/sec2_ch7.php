3e5d5ed8c49804a22a224ba6a3c8feed3d4f6d19
MizukiNumata <handle1997@gmail.com>
2020-09-22 04:21:27 +0900

    add ch7 109

---
+++ article/sec2_ch7.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Sec. 2 Ch. 7.</title>
+    <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+    <div class="column">
+        <div class="article" lang="en">
+            <h2>
+                <sCap>Chapter</sCap> VII. Genitive. <a href="#ch7-e1">(*1)</a>
+            </h2>
+            <p id="ch7-e1" type="note">
+                (*1) <br>
+                On the genitive in Sanskrit, especially in the dialect of the Ṛgvedamantras, there exists a monography of Dr. <sCap>Siecke</sCap> <cite>de genitivo in lingua Sanscritica imprimis Vedica usu</cite>, Berlin, 1869.
+            </p>
+        </div>
+        <div class="article" lang="ja">
+            <h2>
+                第7章 属格（genitive）<a href="ch7-j1">（*1）</a>
+            </h2>
+            <p id="ch7-j1" type="note">
+                （*1）<br>
+                サンスクリット、とりわけṚg-vedaのマントラの方言におけるgen.については、Siecke博士の論文<cite>De Genetivi in Lingua Sanscrita Imprimis Vedica Usu</cite>（Berlin, 1869）がある。
+            </p>
+        </div>
+    </div>
+
+    <?php include("sec2/ch7/109.html"); ?>
+
+
+</body>
+
+</html>
