05551fdf1605b1107ecb02e7179e5e381e2e74e2
MizukiNumata <handle1997@gmail.com>
2020-10-09 17:29:55 +0900

    add sec8

---
+++ article/sec2_ch8.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Sec. 2 Ch. 7.</title>
+    <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+    <div class="column">
+        <div class="article" lang="en">
+            <h2>
+                <sCap>Chapter</sCap> VIII. Locative. <a href="#ch8-e1">(*1)</a>
+            </h2>
+            <p id="ch7-e1" type="note">
+                (*1) <br>
+                See <sCap>Delbrück</sCap> <cite>Ablativ, Localis, Instrumentalis</cite> p. 27-49.
+            </p>
+        </div>
+        <div class="article" lang="ja">
+            <h2>
+                第8章 処格（locative）<a href="ch8-j1">（*1）</a>
+            </h2>
+            <p id="ch7-j1" type="note">
+                （*1）<br>
+                Delbrück [1867], p.27-49をみよ。
+            </p>
+        </div>
+    </div>
+
+    <?php include("sec2/ch8/109.html"); ?>
+
+
+
+
+</body>
+
+</html>
