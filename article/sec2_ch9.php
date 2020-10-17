b0d57389b5b482ec45775f7e583d2cdd6f0abdf9
MizukiNumata <handle1997@gmail.com>
2020-10-18 01:59:27 +0900

    create ch9

---
+++ article/sec2_ch9.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Sec. 2 Ch. 9.</title>
+    <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+    <div class="column">
+        <div class="article" lang="en">
+            <h2>
+                <sCap>Chapter</sCap> IX. Periphrastic expression of case-relations.
+            </h2>
+        </div>
+        <div class="article" lang="ja">
+            <h2>
+                第9章 格-関係の迂言表現
+            </h2>
+        </div>
+    </div>
+
+    <?php include("sec2/ch9/151.html"); ?>
+
+
+</body>
+
+</html>
