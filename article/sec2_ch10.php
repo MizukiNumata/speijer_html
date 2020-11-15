248f4fba1591678c1392612f24c4fb57fb9fac61
MizukiNumata <handle1997@gmail.com>
2020-11-16 05:17:19 +0900

    create sec2-ch10

---
+++ article/sec2_ch10.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Sec. 2 Ch. 10.</title>
+    <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+    <div class="column">
+        <div class="article" lang="en">
+            <h2>
+                <sCap>Chapter</sCap> X. Compounds.
+            </h2>
+        </div>
+        <div class="article" lang="ja">
+            <h2>
+                第10章 複合語
+            </h2>
+        </div>
+    </div>
+
+    <?php include("sec2/ch10/204.html"); ?>
+
+
+
+</body>
+
+</html>
