1f251ead46fc2d341d9624beaab5af9b9f24686d
MizukiNumata <handle1997@gmail.com>
2020-11-25 21:32:40 +0900

    add 256

---
+++ article/sec3_ch2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sec. 3 Ch. 2.</title>
+  <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Chapt</scap>. II. Pronouns.
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        第2章 代名詞
+      </h2>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="article" lang="en">
+      <h3>
+        1. <scap>Personal pronouns and their possessives</scap>.
+      </h3>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        1. 人称代名詞とその所有格
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec3/ch2/1/256.html"); ?>
+
+
+</body>
+
+</html>
