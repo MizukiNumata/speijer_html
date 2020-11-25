014a774f7f7c4da4acd64ec3c18918722c029f22
MizukiNumata <handle1997@gmail.com>
2020-11-25 13:18:20 +0900

    add 233

---
+++ article/sec3_ch1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sec. 3 Ch. 1.</title>
+  <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+  <!-- sec -->
+  <div class="column">
+    <div class="article" lang="en">
+      <h1>
+        SECTION III. <br />
+        ON THE DIFFERENT CLASSES OF NOUNS AND PRONOUNS.
+      </h1>
+    </div>
+    <div class="article" lang="ja">
+      <h1>
+        第3部<br>
+        名詞・代名詞の種類について
+      </h1>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Chapt</scap>. I. Substantive. Adjective. Adverb.
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        第1章 実名詞・形容詞・副詞
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec3/233.html"); ?>
+
+
+
+</body>
+
+</html>
