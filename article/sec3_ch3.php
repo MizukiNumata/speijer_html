af046928c3bc321bb4d062841f029c92d0670d72
MizukiNumata <handle1997@gmail.com>
2020-11-29 05:43:00 +0900

    add 292-301

---
+++ article/sec3_ch3.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sec. 3 Ch. 3.</title>
+  <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Chapt</scap>. III. On nouns of number.
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        第3章 数詞について
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec3/ch3/293.html"); ?>
+  <?php include("sec3/ch3/293.html"); ?>
+  <?php include("sec3/ch3/294.html"); ?>
+  <?php include("sec3/ch3/295.html"); ?>
+  <?php include("sec3/ch3/296.html"); ?>
+  <?php include("sec3/ch3/297.html"); ?>
+  <?php include("sec3/ch3/298.html"); ?>
+  <?php include("sec3/ch3/299.html"); ?>
+  <?php include("sec3/ch3/300.html"); ?>
+  <?php include("sec3/ch3/301.html"); ?>
+
+</body>
+
+</html>
