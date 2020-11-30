9e7553bebb2e41160aa7d7fbda3044de0acdc4cc
MizukiNumata <handle1997@gmail.com>
2020-12-01 00:24:05 +0900

    add 314-320

---
+++ article/sec4_ch2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sec. 3 Ch. 1.</title>
+  <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Chapt</scap>. II. On voices.
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        第2章 態（voice）について
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec4/ch2/314.html"); ?>
+  <?php include("sec4/ch2/315.html"); ?>
+  <?php include("sec4/ch2/316.html"); ?>
+  <?php include("sec4/ch2/317.html"); ?>
+  <?php include("sec4/ch2/318.html"); ?>
+  <?php include("sec4/ch2/319.html"); ?>
+  <?php include("sec4/ch2/320.html"); ?>
+
+</body>
+
+</html>
