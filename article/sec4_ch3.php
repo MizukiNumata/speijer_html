6d417fc61283c6203c1dc61d871d6aad3b729cea
MizukiNumata <handle1997@gmail.com>
2020-12-01 04:41:43 +0900

    add 321-331

---
+++ article/sec4_ch3.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sec. 3 Ch. 1.</title>
+  <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Chapt</scap>. III. Tenses and moods.
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        第3章 時制（tense）と法（mood）
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec4/ch3/321.html"); ?>
+  <?php include("sec4/ch3/322.html"); ?>
+
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Present</scap>. (<skt>laṭ</skt>).
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        現在時制（<skt>laṭ</skt>）
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec4/ch3/pres/323.html"); ?>
+  <?php include("sec4/ch3/pres/324.html"); ?>
+  <?php include("sec4/ch3/pres/325.html"); ?>
+  <?php include("sec4/ch3/pres/326.html"); ?>
+  <?php include("sec4/ch3/pres/327.html"); ?>
+
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Past tenses</scap>.
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        過去時制
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec4/ch3/past/328.html"); ?>
+  <?php include("sec4/ch3/past/329.html"); ?>
+  <?php include("sec4/ch3/past/330.html"); ?>
+  <?php include("sec4/ch3/past/331.html"); ?>
+
+</body>
+
+</html>
