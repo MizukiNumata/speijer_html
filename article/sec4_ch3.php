3078d29afacc3668bc462d8548b313b96483a2e7
MizukiNumata <handle1997@gmail.com>
2020-12-02 18:50:15 +0900

    add -348

--- article/sec4_ch3.php
+++ article/sec4_ch3.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>Sec. 3 Ch. 1.</title>
+  <title>Sec. 4 Ch. 3.</title>
   <link rel="stylesheet" type="text/css" href="article.css" />
 </head>
 
---
+++ article/sec4_ch4.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sec. 4 Ch. 4.</title>
+  <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Chapt</scap>. IV. Tenses and moods (<i>continued</i>).
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        第4章 時制と法（承前）
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec4/ch4/342.html"); ?>
+  <?php include("sec4/ch4/343.html"); ?>
+  <?php include("sec4/ch4/344.html"); ?>
+  <?php include("sec4/ch4/344x.html"); ?>
+  <?php include("sec4/ch4/344xx.html"); ?>
+  <?php include("sec4/ch4/345.html"); ?>
+  <?php include("sec4/ch4/346.html"); ?>
+  <?php include("sec4/ch4/347.html"); ?>
+
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Imperative</scap>.
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        命令法
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec4/ch4/ipv/348.html"); ?>
+
+</body>
+
+</html>
