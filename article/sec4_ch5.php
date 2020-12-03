92f35a1e5dd27fee137146012eba1fb1581170b5
MizukiNumata <handle1997@gmail.com>
2020-12-04 06:06:57 +0900

    add 358-369

---
+++ article/sec4_ch5.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sec. 4 Ch. 5.</title>
+  <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Chapt</scap>. V. Participles and participial idioms.
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        第5章 分詞とそのイディオム
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec4/ch5/358.html"); ?>
+  <?php include("sec4/ch5/359.html"); ?>
+  <?php include("sec4/ch5/360.html"); ?>
+  <?php include("sec4/ch5/361.html"); ?>
+  <?php include("sec4/ch5/362.html"); ?>
+  <?php include("sec4/ch5/363.html"); ?>
+  <?php include("sec4/ch5/364.html"); ?>
+
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Absolute cases</scap>.
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        絶対処格・属格
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec4/ch5/abs/365.html"); ?>
+  <?php include("sec4/ch5/abs/366.html"); ?>
+  <?php include("sec4/ch5/abs/367.html"); ?>
+  <?php include("sec4/ch5/abs/368.html"); ?>
+
+</body>
+
+</html>
