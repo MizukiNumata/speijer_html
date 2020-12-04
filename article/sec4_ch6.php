71084639655878dfde213c33606efd1cedc99937
MizukiNumata <handle1997@gmail.com>
2020-12-05 04:19:51 +0900

    add 378-382

--- article/sec4_ch6.php
+++ article/sec4_ch6.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>Sec. 4 Ch. 5.</title>
+  <title>Sec. 4 Ch. 6.</title>
   <link rel="stylesheet" type="text/css" href="article.css" />
 </head>
 
@@ -11,79 +11,20 @@
   <div class="column">
     <div class="article" lang="en">
       <h2>
-        <scap>Chapt</scap>. V. Participles and participial idioms.
+        <scap>Chapt</scap>. VI. Gerunds.
       </h2>
     </div>
     <div class="article" lang="ja">
       <h2>
-        第5章 分詞とそのイディオム
+        第6章 絶対分詞（動名詞）
       </h2>
     </div>
   </div>
 
-  <?php include("sec4/ch5/358.html"); ?>
-  <?php include("sec4/ch5/359.html"); ?>
-  <?php include("sec4/ch5/360.html"); ?>
-  <?php include("sec4/ch5/361.html"); ?>
-  <?php include("sec4/ch5/362.html"); ?>
-  <?php include("sec4/ch5/363.html"); ?>
-  <?php include("sec4/ch5/364.html"); ?>
-
-  <div class="column">
-    <div class="article" lang="en">
-      <h2>
-        <scap>Absolute cases</scap>.
-      </h2>
-    </div>
-    <div class="article" lang="ja">
-      <h2>
-        絶対処格・属格
-      </h2>
-    </div>
-  </div>
-
-  <?php include("sec4/ch5/abs/365.html"); ?>
-  <?php include("sec4/ch5/abs/366.html"); ?>
-  <?php include("sec4/ch5/abs/367.html"); ?>
-  <?php include("sec4/ch5/abs/368.html"); ?>
-  <?php include("sec4/ch5/abs/369.html"); ?>
-  <?php include("sec4/ch5/abs/370.html"); ?>
-  <?php include("sec4/ch5/abs/371.html"); ?>
-  <?php include("sec4/ch5/abs/372.html"); ?>
-
-  <div class="column">
-    <div class="article" lang="en">
-      <h2>
-        <scap>Other participial idioms</scap>.
-      </h2>
-    </div>
-    <div class="article" lang="ja">
-      <h2>
-        その他の分詞イディオム
-      </h2>
-    </div>
-  </div>
-
-  <?php include("sec4/ch5/other/373.html"); ?>
-  <?php include("sec4/ch5/other/374.html"); ?>
-  <?php include("sec4/ch5/other/375.html"); ?>
-
-  <div class="column">
-    <div class="article" lang="en">
-      <h2>
-        <scap>Participles attended by auxiliaries</scap>.
-      </h2>
-    </div>
-    <div class="article" lang="ja">
-      <h2>
-        助動詞を伴う分詞
-      </h2>
-    </div>
-  </div>
-
-  <?php include("sec4/ch5/pt/376.html"); ?>
-  <?php include("sec4/ch5/pt/377.html"); ?>
-  <?php include("sec4/ch5/pt/378.html"); ?>
+  <?php include("sec4/ch6/379.html"); ?>
+  <?php include("sec4/ch6/380.html"); ?>
+  <?php include("sec4/ch6/381.html"); ?>
+  <?php include("sec4/ch6/382.html"); ?>
 
 </body>
 
