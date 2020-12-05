b163ee2ffc5d8a42221c0d33e7763a3c5b19f077
MizukiNumata <handle1997@gmail.com>
2020-12-06 06:58:16 +0900

    add -397

---
+++ article/sec4_ch7.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sec. 4 Ch. 7.</title>
+  <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Chapt</scap>. VII. Infinitive.
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        第7章 不定詞
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec4/ch6/383.html"); ?>
+  <?php include("sec4/ch6/384.html"); ?>
+  <?php include("sec4/ch6/385.html"); ?>
+  <?php include("sec4/ch6/386.html"); ?>
+  <?php include("sec4/ch6/387.html"); ?>
+  <?php include("sec4/ch6/388.html"); ?>
+  <?php include("sec4/ch6/389.html"); ?>
+  <?php include("sec4/ch6/390.html"); ?>
+  <?php include("sec4/ch6/391.html"); ?>
+  <?php include("sec4/ch6/392.html"); ?>
+  <?php include("sec4/ch6/393.html"); ?>
+
+
+</body>
+
+</html>
---
+++ article/sec5_ch1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Sec. 5 Ch. 1.</title>
+  <link rel="stylesheet" type="text/css" href="article.css" />
+</head>
+
+<body>
+  <!-- sec -->
+  <div class="column">
+    <div class="article" lang="en">
+      <h1>
+        SECTION V. <br />
+        SYNTAX OF THE PARTICLES.
+      </h1>
+    </div>
+    <div class="article" lang="ja">
+      <h1>
+        第5部<br>
+        不変化辞
+      </h1>
+    </div>
+  </div>
+
+  <?php include("sec5/394.html"); ?>
+
+  <div class="column">
+    <div class="article" lang="en">
+      <h2>
+        <scap>Chapt</scap>. I. Particles of emphasis and limitation.
+      </h2>
+    </div>
+    <div class="article" lang="ja">
+      <h2>
+        第1章 強調・限定の不変化辞
+      </h2>
+    </div>
+  </div>
+
+  <?php include("sec5/ch1/395.html"); ?>
+  <?php include("sec5/ch1/396.html"); ?>
+  <?php include("sec5/ch1/397.html"); ?>
+
+
+</body>
+
+</html>
